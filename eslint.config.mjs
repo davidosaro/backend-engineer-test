adf57f8ad990bcf3988c1fb26dd5c9929699833d
David Osaro <157607236+DavidOsaroPeerless@users.noreply.github.com>
2025-03-27 12:53:39 +0100

    feat: initialise app project

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.node } },
+  tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-var-requires": "off",
+    },
+  },
+]);
